7a3c1324ab19575fd4dc74a15f724507df1b5634
abhirana211098 <121804764+abhirana211098@users.noreply.github.com>
2023-03-17 10:27:58 +0530

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region  = "ap-south-1"
+  access_key=""
+  secret_key=""
+  
+}
+
+resource "aws_instance" "my-machine" {
+  ami           = "ami-0d81306eddc614a45"
+  instance_type = "t2.micro"
+  security_groups=["launch-wizard-2"]
+  associate_public_ip_address = true
+  key_name = "searchheadkey"
+  count = 6
+
+  tags = {
+   Name = "${element(var.instance_name,count.index)}"
+    OS = "Linux"
+  }
+
+  }
